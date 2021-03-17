af2b2b78b0a7c5fececcf46d87edd9b28e3fad45
Jordan D. Nelson <jordan.nelson@metova.com>
2021-03-16 22:52:14 -0500

    Use environment variables

--- BNAWineClubApi/Controllers/UsersController.cs
+++ BNAWineClubApi/Controllers/UsersController.cs
@@ -19,9 +19,9 @@ namespace BNAWineClubApi.Controllers
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult Create(User user)
         {
-            if (user.Password == "itsadecanternotamixingbowl"
-                || user.Password == "onceacarignanalwaysacarignan"
-                || user.Password == "fatboyorcoldbrew")
+            if (user.Password == Environment.GetEnvironmentVariable("PASSPHRASE_1")
+                || user.Password == Environment.GetEnvironmentVariable("PASSPHRASE_2")
+                || user.Password == Environment.GetEnvironmentVariable("PASSPHRASE_3"))
             {
                 user.Id = 1;
                 return new CreatedResult(new Uri("https://phy53ofxp0.execute-api.us-east-2.amazonaws.com/Prod/api/users/1"), user);
